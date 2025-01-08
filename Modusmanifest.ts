1d413d27b3057b6ff0994d03ba1a5d1b516816b1
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-01-08 18:52:17 +0200

    Update Modusmanifest.ts

--- Modusmanifest.ts
+++ Modusmanifest.ts
@@ -1,3 +1,4 @@
 import { collections } from "@hypermode/modus-sdk-as"
 import { models } from "@hypermode/modus-sdk-as"
 import { EmbeddingsModel } from "@hypermode/modus-sdk-as/models/experimental/embeddings"
+
