877911f4fb9e27cbfb6843df8745f2ea87e3ae98
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-01-15 10:55:26 +0200

    Update Maximum value of an array.py

--- Maximum value of an array.py
+++ Maximum value of an array.py
@@ -1,9 +1,16 @@
-def maxval(arr):
-  maxval = arr[0]
-  maxval_index = 0
-  for i in range(len(arr)):
-    if arr[i] > maxval:
-      maxval = arr[i]
-      maxval_index = i
-  return maxval_index
+# finding the Maximum value in an array
+# i represents the value of each element.
+
+
+def max_val(array):
+    maxval = array[0]]
+    maxval_index = 0
+    for i in range(len(array)):  # this is fine for looping over the elements in the array.
+        if array[i] > maxval: # If the element is greater than the initalized maxval, we find get the element.
+            maxval = array[i]
+            maxval_index = i
+    return maxval
+
+array = [1,2,6,8,9,-1]
+print("the largest value is at index : ", max_val(array))
       
