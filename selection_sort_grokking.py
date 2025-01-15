b798303edc7ec436f394917f59b0facf7ad258e0
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-01-15 11:28:52 +0200

    Create selection_sort_grokking.py

---
+++ selection_sort_grokking.py
@@ -0,0 +1,12 @@
+def minimum_value(arr):
+  min_val = arr[0]
+  min_val_index = 0
+
+  for i in range(1, len(arr)):
+    if arr[i] < min_val:
+      min-val = arr[i]
+      min_val_index = i
+  return min_val_index
+
+def selectionsort(arr):
+  
