8779d30d2da78ed77e98967de64d0338a80bebc8
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-01-15 11:29:20 +0200

    Update Minimum Value of an Array.py

--- Minimum Value of an Array.py
+++ Minimum Value of an Array.py
@@ -5,7 +5,7 @@ def find_min(arr):
     min_val = arr[0]
     min_val_index = 0
 
-    for i in range(len(arr)):
+    for i in range(1, len(arr)):
         if arr[i] < min_val:
             min_val = arr[i]
             min_val_index = i
