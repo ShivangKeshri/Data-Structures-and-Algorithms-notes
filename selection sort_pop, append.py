25ee7dc441827d3d493a73f0c30901fa7289ba5f
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-01-17 13:28:19 +0200

    Update selection sort_pop, append.py

--- selection sort_pop, append.py
+++ selection sort_pop, append.py
@@ -1,1 +1,10 @@
+def selection_sort(arr):
+  for i in range(0, len(arr)-1):
+    cur_min_index = i
+    for j in range(i+1, len(arr)):
+      if arr[j] < arr[cur_min_index]:
+        cur_min_index = j 
+    min_value = arr.pop(cur_min_index)
+    arr.insert(i, min_value)
 
+print(selectionsort(arr))
