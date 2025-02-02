1288c00b72d604f18a8db8709f1dd5088d35e6d5
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-02-02 09:35:12 +0200

    Create Euclidean Algorithm.py

---
+++ Euclidean Algorithm.py
@@ -0,0 +1,18 @@
+def euclidean_algo(a,b):
+  while b!=0:
+    remainder = a % b
+    print(f"{a} = {a//b}*{b} + {remainder})
+    a = b
+    b = remainder
+  return a
+
+a = 120
+b = 25
+print("The Euclidean algorithm using division:\n")
+print(f"The GCD of {a} and {b} is: {euclidean_algorithm(a, b)}")
+
+# output : 
+120=4*25 + 20
+25=1*20 + 5
+20=4*5 + 0
+The GCD of 120 and 25 is: 5
