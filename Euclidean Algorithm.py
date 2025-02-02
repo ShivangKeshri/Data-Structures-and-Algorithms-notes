f78b3670fef77da1acaa9942abdbcc358688052d
Shivang Keshri <155295831+ShivangKeshri@users.noreply.github.com>
2025-02-02 09:35:38 +0200

    Update Euclidean Algorithm.py

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -12,7 +12,7 @@ print("The Euclidean algorithm using division:\n")
 print(f"The GCD of {a} and {b} is: {euclidean_algorithm(a, b)}")
 
 # output : 
-120=4*25 + 20
-25=1*20 + 5
-20=4*5 + 0
-The GCD of 120 and 25 is: 5
+# 120=4*25 + 20
+# 25=1*20 + 5
+# 20=4*5 + 0
+# The GCD of 120 and 25 is: 5
